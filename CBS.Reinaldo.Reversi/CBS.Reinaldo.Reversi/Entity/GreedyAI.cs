e7ddbe9e6c097a5eca3cae22b5be4f20449fd7a3
reinaldo.hasian <reinaldo.hasian@xtremax.com>
2019-09-10 19:17:02 +0700

    add greedy AI

---
+++ CBS.Reinaldo.Reversi/CBS.Reinaldo.Reversi/Entity/GreedyAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CBS.Reinaldo.Reversi.Entity
+{
+    public class GreedyAI : Player
+    {
+        public void AutoMove(IEnumerable<Button> board)
+        {
+            Button maxPanel = new Button()
+            {
+                Text = "0"
+            };
+            foreach(var panel in board)
+            {
+                var maxPanelCount = int.Parse(maxPanel.Text);
+                int panelCount;
+                try
+                {
+                    panelCount = int.Parse(panel.Text);
+                }
+                catch
+                {
+                    continue;
+                }
+
+                if (panelCount >= maxPanelCount) maxPanel = panel;
+            }
+
+            //Thread.Sleep(1000);
+            if (maxPanel != null) maxPanel.PerformClick();
+            return;
+        }
+    }
+}
